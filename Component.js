ac9688756ee9806cc8c92a7b6257ea5da2c10caf
uuganbat gantumur <kite@pop-os.localdomain>
2021-03-18 03:17:46 +0800

    refactored and added App.js template

---
+++ Component.js
@@ -0,0 +1,69 @@
+
+let [ NEW_LINE, TAB ] = ['\n', '\t'];
+module.exports.Component = class Component {
+	static screenImports = '';
+	imports = [];
+	importStrings = '';
+	componentStrings = '';
+	name = '';
+	isScreen;
+	hasInputField = false;
+
+	constructor(
+		name,
+		imports,
+		isScreen,
+		hasInputField
+	) {
+		name = name[0].toUpperCase().concat(name.slice(1,name.length));
+		if (isScreen) {
+			name = name.concat('Screen');
+			Component.screenImports +=`import ${name} from './screens/${name}';${NEW_LINE}`;
+		};
+		if (hasInputField != undefined) {
+			this.hasInputField = hasInputField;
+		}
+		this.name = name;
+		this.isScreen = isScreen;
+		this.imports = imports;
+	}
+
+	fillTemplateAndReturn = () => {
+		if (this.imports[0]) {
+			this.imports.map(importName => {
+				importName = importName[0].toUpperCase().concat(importName.slice(1,importName.length));
+				this.importStrings += `import ${importName} from '..components/${importName}';${NEW_LINE}`
+				this.componentStrings += `${TAB}${TAB}${TAB}${TAB}<${importName}/>${NEW_LINE}`
+			} 
+			)
+		}
+
+		return (
+`import React, { useState, useEffect } from 'react';
+import { StyleSheet, ${hasInputField ? 'PlatForm, KeyboardAvoidingView ': ''}Text, View } from 'react-native';
+${this.importStrings}
+
+export default function ${this.name}() {
+
+	return (
+			<${ hasInputField ?'KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}' : 'View'} style={styles.container}>
+				<Text>
+					${this.name}
+				</Text>
+${this.componentStrings}
+			</${ hasInputField ?'KeyboardAvoidingView' : 'View'}>
+	)
+
+}
+
+let styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'rgb( ${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)} )',
+	}
+})`
+		)
+	}
+}
--- haku.js
+++ haku.js
@@ -3,63 +3,7 @@ let [ NEW_LINE, TAB ] = ['\n', '\t'];
 
 let inputJson = fs.readFileSync('haku.json');
 let structure = JSON.parse(inputJson);
-
-class Component {
-	imports = [];
-	importStrings = '';
-	componentStrings = '';
-	name = '';
-	isScreen;
-	constructor(name, imports, isScreen) {
-		name = name[0].toUpperCase().concat(name.slice(1,name.length));
-		if (isScreen) { name = name.concat('Screen') };
-		this.isScreen = isScreen;
-		this.name = name;
-		this.imports = imports;
-	}
-
-	fillTemplateAndReturn = () => {
-		if (this.imports[0]) {
-			this.imports.map(importName => {
-				importName = importName[0].toUpperCase().concat(importName.slice(1,importName.length));
-				this.importStrings += `import ${importName} from 'components/${importName}';${NEW_LINE}`
-				this.componentStrings += `${TAB}${TAB}${TAB}${TAB}<${importName}/>${NEW_LINE}`
-			} 
-			)
-		}
-		for (let i in this.imports) {
-			console.log(this.imports[i]);
-		}
-		return (
-`import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-${this.importStrings}
-
-export default function ${this.name}() {
-
-	return (
-			<View style={styles.container}>
-				<Text>
-					${this.name}
-				</Text>
-${this.componentStrings}
-			</View>
-	)
-
-}
-
-let styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'rgb( ${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)} )',
-	}
-})`
-		)
-	}
-}
-
+let { Component } = require('./Component');
 
 let components = 'components';
 let screens = 'screens';
@@ -77,10 +21,69 @@ let giveFileName = (folderName, fileName, isScreen) => {
 }
 
 structure.screens.map(file => {
-	fs.appendFileSync(giveFileName(screens, file.name, true), new Component(file.name, file.imports, true).fillTemplateAndReturn());
+	fs.appendFileSync(
+		giveFileName(
+			fodlerName = screens,
+			fileName = file.name, 
+			isScreen = true
+		),
+		new Component(
+			name = file.name,
+			imports = file.imports,
+			isScreen = true,
+			hasInputField = file.hasInputField
+		).fillTemplateAndReturn()
+	);
 });
 
 structure.components.map(file => {
-	fs.appendFileSync(giveFileName(components, file.name, false), new Component(file.name, file.imports, false).fillTemplateAndReturn());
+	fs.appendFileSync(
+		giveFileName(
+			fodlerName = components,
+			fileName = file.name, 
+			isScreen = false
+		),
+		new Component(
+			name = file.name,
+			imports = file.imports,
+			isScreen = false,
+		).fillTemplateAndReturn()
+	);
 });
+
+
+let appFileTemplate = 
+`import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+${Component.screenImports}
+
+import { NavigationContainer } from '@react-navigation/native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+		<NavigationContainer>
+		  <Text style={styles.text}>Appjs</Text>
+		</NavigationContainer>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+	paddingTop: Constants.statusBarHeight
+  },
+  
+  text: {
+	color: 'black'
+  },
+  
+});`
+
+fs.appendFileSync('App.js', appFileTemplate);
 
